248d3f58d8c129131ef93a5badbc693f77de2651
rgonzalezrdam1 <rgonzalezrdam1@gmail.com>
2020-04-17 13:37:46 +0200

    prueba

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
 		System.out.println("Probando Git desde eclipse");
 		System.out.println("segundo cambio");
 		System.out.println("tercer cambio");
-		System.out.println("cuarto cambio");
 	}
 
 }
